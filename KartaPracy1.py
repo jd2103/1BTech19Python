3027c747515edaccdf3db5f4eecc298eccee809f
1B5JD <114583639+jd2103@users.noreply.github.com>
2022-10-05 12:12:24 +0000

    gotowa karta pracy

--- KartaPracy1.py
+++ KartaPracy1.py
@@ -1,11 +1,41 @@
 # Zadanie 1
 
-a= int(input())
-b= int(input())
-print(a**2 + b**2)
+# a= int(input())
+# b= int(input())
+# print(a**2 + b**2)
 
 # Zadanie 2
 
+# a= int(input())
+# b= int(input())
+# print((a+b)**2)
+
+# Zadanie 3
+
+# a= int(input())
+# b= int(input())
+# print((a-b)**3)
+
+#Zadanie 4
+
+# a= int(input())
+# b= int(input())
+# c= int(input())
+# print(a*b*c)
+
+#Zadanie 5
+
+# a= int(input())
+# b= int(input())
+# print(2*(a+b)/5)
+
+#Zadanie 6
+
+# brutto= float(input())
+# print(brutto / 1.23)
+
+#Zadanie 7
+
 a= int(input())
 b= int(input())
-print((a+b)**2)
+print(a%b)
