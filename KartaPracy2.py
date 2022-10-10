d1e754cc5b9dd7c31537d6e126a3edc61c4b7bae
1B5JD <114583639+jd2103@users.noreply.github.com>
2022-10-10 10:20:46 +0000

    Karta pracy 2 zdania 1-4

---
+++ KartaPracy2.py
@@ -0,0 +1,29 @@
+# #Zad 1
+# n = int(input())
+
+# if n % 3 == 0:
+#   print("TAK")
+# else:
+#   print("NIE")
+  
+#Zad 2 
+n = int(input())
+if n >= 100 and n < 1000 and n % 17 == 0:
+    print("TAK")
+else:
+    print("NIE")
+
+#Zad 3
+# wiek = int(input())
+# if wiek >= 18:
+#     print("TAK jesteś pełnoletni")
+# else:
+#     print("NIE jesteś")
+
+#Zad 4 
+LIMIT = 20
+waga int(input())
+if waga <= LIMIT:
+  print("JEDZ spokojnie")
+else: 
+  print("LEPIEJ nie jedz")
