d479088209176a2818c51a4ceab956be005e5ea5
1B5JD <114583639+jd2103@users.noreply.github.com>
2022-10-12 12:11:21 +0000

    skończona karta pracy 2

--- KartaPracy2.py
+++ KartaPracy2.py
@@ -6,12 +6,12 @@
 # else:
 #   print("NIE")
   
-#Zad 2 
-n = int(input())
-if n >= 100 and n < 1000 and n % 17 == 0:
-    print("TAK")
-else:
-    print("NIE")
+# #Zad 2 
+# n = int(input())
+# if n >= 100 and n < 1000 and n % 17 == 0:
+#     print("TAK")
+# else:
+#     print("NIE")
 
 #Zad 3
 # wiek = int(input())
@@ -21,9 +21,37 @@ else:
 #     print("NIE jesteś")
 
 #Zad 4 
-LIMIT = 20
-waga int(input())
-if waga <= LIMIT:
-  print("JEDZ spokojnie")
-else: 
-  print("LEPIEJ nie jedz")
+# LIMIT = 20
+# waga int(input())
+# if waga <= LIMIT:
+#   print("JEDZ spokojnie")
+# else: 
+#   print("LEPIEJ nie jedz")
+
+#Zad 5
+# a = int(input())
+# b = int(input())
+# c = int(input())
+
+# if (c>a and c<b) or (c>b and c<a):
+#     print("TAK")
+# else:
+#     print("NIE")
+
+#Zad 6
+# a = int(input())
+# p = int(input())
+# if (a**p-a) % p == 0:
+#     print("TAK, spełnia MTF")
+# else:
+#   print("NIE, nie spełnia MTF")
+
+#Zad 7
+# p = int(input())
+# k = int(input())
+# s = int(input())
+# o=k-s
+# if o/s<=3:
+#   print("TAK")
+# else:
+#   print("NIE")
