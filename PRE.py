f44dbca65d6a3468ca455a434fc37f9c8e1b25ab
1B5JD <114583639+jd2103@users.noreply.github.com>
2022-10-19 12:15:55 +0000

    KartaPracy3 zadania 1-2

---
+++ PRE.py
@@ -0,0 +1,28 @@
+# PRE
+
+# pętla liczb dwucyfrowych od 10 do 21
+# pętla liczb dwucyfrowych nieparzystych od 15 do 31  
+# pętla liczb dwucyfrowych malejąco (step ujemny)
+# pętla liczb dwucyfrowych malejąco (step dodatni)
+# pętla liczb 3-cyfrowych podzielnych przez 20
+
+#Zad1
+# for i in range(10,22):
+#   print(i)
+
+#Zad2
+# for i in range(15,32,2):
+#   print(i)
+
+#Zad3
+# for i in range(99,9,-1):
+#   print(i, end=" ")
+
+#Zad4
+# for i in range(10,100,1):
+#   print(109-i, end=" ")
+
+#Zad5
+# for i in range(100,1000,20):
+#   print(i, end=" ")
+
