c4d2852a9297f36794134976bfaf9dd82e435b63
1B5JD <114583639+jd2103@users.noreply.github.com>
2022-10-23 11:54:38 +0000

    KartaPracy3b zadania 1-4

---
+++ KartaPracy3b.py
@@ -0,0 +1,26 @@
+#Zad1
+# for i in range(1,31):
+#   print(i, end=" ")
+
+#Zad2
+# for i in range(1,10,2):
+#   print(i**2, end=" ")
+
+#Zad3
+# for i in range(379,10000,379):
+#   if i>1000:
+#     print(i, end=" ")
+
+#Zad4
+for i in range(100,1000):
+  if (i/5 or i/6 or i/11):
+    print(i, end=" ")
+  
+
+
+
+
+
+
+
+
