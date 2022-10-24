0408624aef1176b1da1432f0f0f96e208d05ac33
1B5JD <114583639+jd2103@users.noreply.github.com>
2022-10-24 10:20:15 +0000

    KartaPracy3 zadania 1-5

--- KartaPracy3.py
+++ KartaPracy3.py
@@ -1,20 +1,30 @@
-#Zad 1
-n = int(input())
+#Zad1
+# n = int(input())
 
-for i in range(n):
-  print(i**3 + 3, end=" ")
+# for i in range(n):
+#   print(i**3 + 3, end=" ")
 
-#Zad 2
-
+#Zad2
 # for i in range(105,1000,15):
 #   print(i, end=" ")
 
+#Zad3
+# n = int(input())
+# for i in range(1,n+1):
+#   if n % i == 0:
+#     print(i, end=" ")
+
+#Zad4
+# suma = 0
+# for i in range(10,100):
+#   suma = suma + i
+#   print(suma)
+
+#Zad5
+n = int(input())
+for i in range(n-1):
+  k = int(input())
   
-
-
-
-
-
 
 
 
