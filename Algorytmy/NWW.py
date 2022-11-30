d61068c398727a74c32796c38a871d61656a5762
1B5JD <114583639+jd2103@users.noreply.github.com>
2022-11-30 13:15:17 +0000

    NWW-odejmowanie, modulo

---
+++ Algorytmy/NWW.py
@@ -0,0 +1,17 @@
+#Odejmowanie
+# a=int(input())
+# b=int(input())
+# iloczyn=a*b
+# while a!=b:
+#     if a>b: a=a-b
+#     if b>a: b=b-a
+# print(iloczyn//a)
+
+#Modulo
+# a=int(input())
+# b=int(input())
+# iloczyn=a*b
+# while b>0:
+#   a,b=b,a%b
+# print(iloczyn//a)
+
