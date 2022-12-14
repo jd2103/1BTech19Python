6324dde6422ff18de8d7307594fdcae7a52d7ce3
1B5JD <114583639+jd2103@users.noreply.github.com>
2022-12-14 13:14:36 +0000

    KartaPracy6 zadania 1-6

--- KartyPracy/KartaPracy6.py
+++ KartyPracy/KartaPracy6.py
@@ -22,11 +22,32 @@
 #         suma+=i
 # print(suma)
 
+#Zadanie 3
+# for i in range(99,9,-1):
+#     if i % 7 == 0:
+#         podzielnik = i
+#         break
+    
+# ilosc = 0
+# for i in range(1000, 10000):
+#     if i % podzielnik == 0:
+#         ilosc += 1
+# print(ilosc)
+
 #Zadanie 4
+# ilosc=0
+# for i in range(10,100):
+#     cd=i//10
+#     cj=i%10
+#     if cd>=2*cj:
+#         ilosc+=1
+# print(ilosc)
+
+#Zadanie 5
 ilosc=0
 for i in range(10,100):
-    cd=i//10
+    cs=i//100
+    cd=(i//10)%10
     cj=i%10
-    if cd>=2*cj:
-        ilosc+=1
+    
 print(ilosc)
