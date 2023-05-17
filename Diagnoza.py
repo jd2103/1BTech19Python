d9c3e64ec25d4026172f8eec264a303fd688120c
JustynaD3 <12503583-JustynaD3@users.noreply.replit.com>
2023-05-17 15:46:47 +0000

    zadania na diagnoze zrobiłam

---
+++ Diagnoza.py
@@ -0,0 +1,60 @@
+#   WSTĘP
+# 1. Oblicz sumę liczb 3-cyfrowych
+# suma=0
+# for i in range(100,1000):
+#   suma+=i
+#   wynik=suma
+# print(suma)
+# 2. Oblicz sume i ilość dwucyfrowych wielokrotności liczby 6
+# suma=0
+# ilosc=0
+# for i in range(10,100):
+#   if i%6==0:
+#     suma+=i
+#     ilosc+=1
+# print("Suma to", suma)
+# print("Ilosc to", ilosc)
+# 3. Znajdź największa liczbę wśród 5 wylosowanych przez program liczb 3-cyfrowych
+# from random import randint
+# L = [randint(99,1000) for i in range(5)]
+# print(L)
+# print(max(L))
+# 4. Podaj sumę cyfr w dowolnej liczbie
+# s=int(input())
+# suma=0
+# while s>0:
+#   suma+=s%10
+#   s//=10
+# print(suma)
+# 5. Znajdź najmniejszą cyfre we wpisanej przez usera liczbie 3-cyfrowej
+# s=int(input())
+# L=list(str(s))
+# print(min(L))
+
+#   ALGORYTMY
+# 1. Sprawdź czy wpisana przez usera liczba jest pierwsza
+# n = int(input())
+# for i in range(2,n):
+#     if n % i == 0:
+#         print("Liczba nie jest pierwsza")
+#         exit(0)
+# print("Liczba jest pierwsza")
+# 2. Sprawdź czy wpisana przez usera liczba jest złożona
+# n = int(input())
+# for i in range(2,n):
+#     if n % i == 0:
+#         print("Liczba jest złożona")
+#         exit(0)
+# print("Liczba jest pierwsza")
+# 3. Sprawdź czy wpisana przez usera liczba jest względnie pierwsza z 24
+# 4. Zakoduj szyfrem Cezara i kluczem k słowo s 
+# 5. Dodaj dwa ułamki a/b + c/d. Zapisz sumę jako ułamek nieskracalny i liczbe mieszaną
+# 6. Znajdź NWW dwóch wpisanych przez usera liczb
+#   KARTKA
+# 1. Oblicz wartość ONP
+# 2. Znajdź postac ONP dla pisanego wyrażenia
+# 3. Napisz algorytm NWD (obie wersje) i przeprowadź symulacje kroków dla podanych liczb
+#   NAPISY
+# 1. Znajdź ilość literki C we wpisanym napisie
+# 2. Sprawdź czy literki w napisie sa w porządku nierosnącym np. ZOO, WOK, WODA itp
+# 3. Podaj najdłuższy z 3 wpisanych przez usera wyrazów
--- Listy/zadanialisty.py
+++ Listy/zadanialisty.py
@@ -1,11 +1,11 @@
 
 # //Wygeneruj tablicę n losowych liczb:
-from random import randint
-L = [randint(1,21) for i in range(20)]
-print(L)
-print(max(L))
-print(min(L))
-print(L.count(max(L)))
+# from random import randint
+# L = [randint(1,21) for i in range(20)]
+# print(L)
+# print(max(L))
+# print(min(L))
+# print(L.count(max(L)))
 # 1. Znajdź największą liczbę w tablicy
 # 2. Znajdź najmniejszą liczbę w tablicy
 # 3. Podaj ile razy występuje najwieksza liczba w tablicy
