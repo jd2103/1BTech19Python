b8cd4252a7de4b87dab6f508ba4f9be351252a6c
jd2103 <114583639+jd2103@users.noreply.github.com>
2023-05-18 15:02:47 +0000

    dodane zadania

--- Diagnoza.py
+++ Diagnoza.py
@@ -47,8 +47,30 @@
 #         exit(0)
 # print("Liczba jest pierwsza")
 # 3. Sprawdź czy wpisana przez usera liczba jest względnie pierwsza z 24
+# a=int(input())
+# if a % 2 or 3 or 4 or 6 or 8 or 12==0:
+#   print("jest względnie pierwsza")
+# else:
+#   print("nie jest")
+#                   DO POPRAWIENIA!!!
 # 4. Zakoduj szyfrem Cezara i kluczem k słowo s 
+
 # 5. Dodaj dwa ułamki a/b + c/d. Zapisz sumę jako ułamek nieskracalny i liczbe mieszaną
+# a=int(input("Licznik nr 1 "))
+# b=int(input("Mianownik nr 1 "))
+# c=int(input("Licznik nr 2 "))
+# d=int(input("Mianownik nr 2 "))
+# x, y = b, d
+# iloczyn=x*y
+# while x!=y:
+#     if x>y: x=x-y
+#     if y>x: y=y-x
+# nww=iloczyn//x
+# e=(nww//b)*a
+# f=(nww//d)*c
+# g=e+f
+#   print(f"{a}/{b} + {c}/{d} = {e}/{nww} + {f}/{nww} = {g}/{nww}")
+  
 # 6. Znajdź NWW dwóch wpisanych przez usera liczb
 #   KARTKA
 # 1. Oblicz wartość ONP
@@ -56,5 +78,12 @@
 # 3. Napisz algorytm NWD (obie wersje) i przeprowadź symulacje kroków dla podanych liczb
 #   NAPISY
 # 1. Znajdź ilość literki C we wpisanym napisie
+# napis = input("Wpisz napis: ")
+# lw= 0
+# for znak in napis:
+#     if znak == "C":
+#         lw += 1
+# print("Ilość wystąpień litery C:", lw)
+
 # 2. Sprawdź czy literki w napisie sa w porządku nierosnącym np. ZOO, WOK, WODA itp
 # 3. Podaj najdłuższy z 3 wpisanych przez usera wyrazów
